f320ac14f804902d6516e2584f6f433395ece134
filipecx <filipecavalcantiximenes@gmail.com>
2024-01-13 12:09:29 -0300

    baseurl back

--- recipes/pages/Home.jsx
+++ recipes/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState} from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Axios from 'axios'
 import {Titulo} from '../components/Titulo'
-const baseUrl = 'https://receitas-five.vercel.app/'
+const baseUrl = 'https://receitasback.vercel.app/'
 
 export function Home(){
     const [receitas, setReceita] = useState([])
